e4a244163f9a7e135cdef2ab0a8b0425bc61c789
907631 <pwitt-c@scrippsnetworks.com>
2018-06-23 21:41:54 -0400

    bug fixes

--- pizero.py
+++ pizero.py
@@ -2,6 +2,7 @@
 
 # import the necessary packages
 import argparse
+import cv2
 import imutils
 import json
 import logging
@@ -13,7 +14,7 @@ import uuid
 from datetime import datetime, timedelta
 from imutils.video import VideoStream
 from picamera.array import PiRGBArray
-from picamera import PiCameraimport cv2
+from picamera import PiCamera
 
 # setting up logging for this script
 _LEVEL = logging.INFO
@@ -68,99 +69,105 @@ def main():
     out = None
 
     # Initialize the window
-    cv2.namedWindow('Security Feed', cv2.WINDOW_NORMAL)
-    #cv2.moveWindow('Security Feed', 0, 0)
+    if conf["show_video"]:
+        cv2.namedWindow('Security Feed', cv2.WINDOW_NORMAL)
+        #cv2.moveWindow('Security Feed', 0, 0)
 
     # capture frames from the camera
     for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        # grab the raw NumPy array representing the image and initialize
-        # the timestamp and occupied/unoccupied text
-        frame = f.array
-        timestamp = datetime.now()
+        try:
+            # grab the raw NumPy array representing the image and initialize
+            # the timestamp and occupied/unoccupied text
+            frame = f.array
+            timestamp = datetime.now()
 
-        # resize the frame, convert it to grayscale, and blur it
-        frame = imutils.resize(frame, width=width, height=height)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+            # resize the frame, convert it to grayscale, and blur it
+            frame = imutils.resize(frame, width=width, height=height)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-        # if the average frame is None, initialize it
-        if avg is None:
-            LOG.info("Starting background model...")
-            rawCapture.truncate(0)
-            avg = gray.copy().astype("float")
-            text = "Unoccupied"
-            video_start = None
-            in_window = False
-            continue
-
-        # accumulate the weighted average between the current frame and
-        # previous frames, then compute the difference between the current
-        # frame and running average
-        cv2.accumulateWeighted(gray, avg, 0.5)
-        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-
-        # threshold the delta image, dilate the thresholded image to fill
-        # in holes, then find contours on thresholded image
-        thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
-        thresh = cv2.dilate(thresh, None, iterations=2)
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
-        # loop over the contours
-        for c in cnts:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < conf["min_area"]:
+            # if the average frame is None, initialize it
+            if avg is None:
+                LOG.info("Starting background model...")
+                rawCapture.truncate(0)
+                avg = gray.copy().astype("float")
+                text = "Unoccupied"
+                video_start = None
+                in_window = False
                 continue
 
-            if conf['draw_boxes']:
-                # compute the bounding box for the contour, draw it on the frame, and update the text
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # accumulate the weighted average between the current frame and
+            # previous frames, then compute the difference between the current
+            # frame and running average
+            cv2.accumulateWeighted(gray, avg, 0.5)
+            frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
-            if not text == "Occupied":
-                text = "Occupied"
-                video_start = datetime.now()
-                filename = get_path(conf['basepath'], conf['ext'], timestamp)
+            # threshold the delta image, dilate the thresholded image to fill
+            # in holes, then find contours on thresholded image
+            thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-                if out:
-                    out.release()
-                    out = None
+            # loop over the contours
+            for c in cnts:
+                # if the contour is too small, ignore it
+                if cv2.contourArea(c) < conf["min_area"]:
+                    continue
 
-                out = cv2.VideoWriter(filename, fourcc, fps, size)
+                if conf['draw_boxes']:
+                    # compute the bounding box for the contour, draw it on the frame, and update the text
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # draw the text and timestamp on the frame
-        ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-        cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+                if not text == "Occupied":
+                    text = "Occupied"
+                    video_start = datetime.now()
+                    filename = get_path(conf['basepath'], conf['ext'], timestamp)
 
-        if video_start:
-            if (timestamp - video_start).total_seconds() < conf['minimum_video_length']:
-                in_window = True
-            else:
-                in_window = False
+                    if out:
+                        out.release()
+                        out = None
 
-        # check to see if the room is occupied
-        if text == "Occupied":
-            out.write(frame)
+                    out = cv2.VideoWriter(filename, fourcc, fps, size)
 
-        if not cnts and not in_window:
-            text = "Unoccupied"
+            # draw the text and timestamp on the frame
+            ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+            cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-        if not in_window:
-            video_start = None
+            if video_start:
+                if (timestamp - video_start).total_seconds() < conf['minimum_video_length']:
+                    in_window = True
+                else:
+                    in_window = False
 
-        # check to see if the frames should be displayed to screen
-        if conf["show_video"]:
-            # display the security feed
-            cv2.imshow("Security Feed", frame)
-            key = cv2.waitKey(1) & 0xFF
+            # check to see if the room is occupied
+            if text == "Occupied":
+                out.write(frame)
 
-        rawCapture.truncate(0)
+            if not cnts and not in_window:
+                text = "Unoccupied"
 
-        # if the `q` key is pressed, break from the lopp
-        if key == ord("q"):
+            if not in_window:
+                video_start = None
+
+            # check to see if the frames should be displayed to screen
+            if conf["show_video"]:
+                # display the security feed
+                cv2.imshow("Security Feed", frame)
+                key = cv2.waitKey(1) & 0xFF
+
+                # if the `q` key is pressed, break from the lopp
+                if key == ord("q"):
+                    break
+
+            rawCapture.truncate(0)
+
+        except KeyboardInterrupt:
             break
 
-    out.release()
+    if out:
+        out.release()
     out = None
     cv2.destroyAllWindows()
 
